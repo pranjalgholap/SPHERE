0293c006545a09e27e306428aa01f1d8166edae7
Pranjal Gholap <93419451+pranjalgholap@users.noreply.github.com>
2022-04-14 18:47:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // Loading
 const textureLoader = new THREE.TextureLoader()
 
-const normalTextureEarth = textureLoader.load('./Earth.jpg')
-const normalTextureSun = textureLoader.load('./Sun.jpg')
+const normalTextureEarth = textureLoader.load('./Earth.png')
+const normalTextureSun = textureLoader.load('./Sun.png')
 
 // Debug
 //const gui = new dat.GUI()
